c471a8e32a8bb267190cb9a3c2048da602026cf6
Enzo Benfica <128191773+enzobenficaa@users.noreply.github.com>
2023-12-12 11:56:52 -0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  port: "3000",
+  username: "postgres",
+  password: "Enzobenfica",
+  database: "curso_sequelize",
+  define: {
+    timestamps: true,
+  },
+};
