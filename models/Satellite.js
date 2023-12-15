509b61ad31fbcf8638543d3d4c1244b638df8ccd
Enzo Benfica <128191773+enzobenficaa@users.noreply.github.com>
2023-12-15 15:40:25 -0300

    Add files via upload

---
+++ models/Satellite.js
@@ -0,0 +1,8 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/sequelize");
+const Satellite = sequelize.define("satellites", {
+  name: DataTypes.STRING,
+  serialNumber: DataTypes.INTEGER,
+  planetId: DataTypes.INTEGER,
+});
+module.exports = Satellite;
