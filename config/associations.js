cbf9b374c0c6bac85ea113736f71cd511728e2c8
Enzo Benfica <128191773+enzobenficaa@users.noreply.github.com>
2023-12-22 16:45:59 -0300

    Add files via upload

--- config/associations.js
+++ config/associations.js
@@ -1,5 +1,24 @@
 const Planet = require("../models/Planet");
-const Satellites = require("../models/Satellite");
-Planet.hasOne(Satellites, { onDelete: "CASCADE", onUpdate: "CASCADE" });
-Satellites.belongsTo(Planet, { foreigKey: "planetId", as: "planet" });
-module.exports = { Planet, Satellites };
+const Satelite = require("../models/Satelite");
+const Cap = require("../models/Cap");
+const Spaceship = require("../models/Spaceship");
+
+Planet.hasOne(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
+Satelite.belongsTo(Planet, { foreignKey: "planetId", as: "planet" });
+
+// Planet.hasMany(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
+// Satelite.belongsTo(Planet, { foreignKey: "planetId", as: "planet" });
+
+Cap.belongsToMany(Spaceship, {
+  foreignKey: "capId",
+  through: "capSpaceship",
+  as: "spaceships",
+});
+
+Spaceship.belongsToMany(Cap, {
+  foreignKey: "spaceshipId",
+  through: "capSpaceship",
+  as: "caps",
+});
+
+module.exports = { Planet, Satelite };
