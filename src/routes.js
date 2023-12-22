f98901ca0ca5bc8207310644eaf507459974e748
Enzo Benfica <128191773+enzobenficaa@users.noreply.github.com>
2023-12-22 16:46:57 -0300

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,23 @@ const express = require("express");
 const routes = express.Router();
 
 const PlanetController = require("../Controller/PlanetController");
-const SatelliteController = require("../Controller/SatelliteController");
+const SateliteController = require("../Controller/SateliteController");
+const CapController = require("../Controller/CapController");
+const SpaceshipController = require("../Controller/SpaceshipController");
 
 // Rotas de Planets
 routes.post("/planets", PlanetController.store);
 routes.get("/planets", PlanetController.index);
 routes.put("/planets/:id", PlanetController.put);
 routes.delete("/planets/:id", PlanetController.delete);
-routes.post("/planets/:planetId/satellites", SatelliteController.store);
-routes.get("/planets/:planetId/satellites", SatelliteController.index);
+
+routes.post("/planets/:planetId/satelites", SateliteController.store);
+routes.get("/planets/:planetId/satelites", SateliteController.index);
+
+routes.get("/cap", CapController.index);
+routes.post("/cap", CapController.store);
+
+routes.post("/caps/:capId/spaceships", SpaceshipController.store);
+routes.get("/caps/:capId/spaceships", SpaceshipController.index);
+
 module.exports = routes;
